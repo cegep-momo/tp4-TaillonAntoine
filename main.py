70c675b49a564cda2ced444b96508fab78a7bbf7
Antoine <2245749@cmontmorency.qc.ca>
2025-04-21 11:06:25 -0400

    FAIT : programme fonctionne, affichage dans console

---
+++ main.py
@@ -0,0 +1,6 @@
+from view.platine import Platine
+
+if __name__ == "__main__":
+    capteur = Platine()
+    capteur.demarrer()
+    
--- view/platine.py
+++ view/platine.py
@@ -1,21 +1,57 @@
-from gpiozero import DistanceSensor
+from gpiozero import DistanceSensor, Button
 from time import sleep
-from ADCDevice import *
+from view.ADCDevice import *
+from view.LCD1602 import CharLCD1602
+from signal import pause
+import sys
 
-capteur = DistanceSensor(echo = 12, trigger = 17, max_distance = 3)
+class Platine:
+    
+    def __init__(self):
+        self.bouton_demarrer = Button(13, bounce_time=0.1)
+        self.bouton_fermer = Button(6, bounce_time=0.1)
+        self.ouvert = False
+        
+        self.ecran_lcd = CharLCD1602()
+        self.ecran_lcd.init_lcd(0x27, 1)
+        
+        self.bouton_fermer.when_pressed = self.fermer_programme
+        
+        
+    
 
-# adc = ADCDevice()
+       
 
-# if(adc.detectI2C(0x4b)):
-#     adc = ADS7830()
+    def mesurer_distance(self):
+        self.capteur = DistanceSensor(echo = 12, trigger = 17, max_distance = 3)
+        while True:
+            cm = self.capteur.distance * 100
+            print(f"Distance : {str(cm)} cm")
+            sleep(1)
+        
+        
+    def potentionmetre(self):
+        self.adc = ADCDevice()
 
-while True:
-    cm = capteur.distance * 100
-    print(f"Distance : {str(cm)} cm")
-    sleep(1)
-
-# while True:
-#     valeurADC = adc.analogRead(0)
-#     voltage = valeurADC / 255.0 * 3.3
-#     print(f"Valeur ADC : {valeurADC}, Voltage :{voltage:.2}")
-#     sleep(0.03)
+        if(self.adc.detectI2C(0x4b)):
+            self.adc = ADS7830()
+            
+        while self.ouvert:
+            self.valeurADC = self.adc.analogRead(0)
+            self.voltage = self.valeurADC / 255.0 * 3.3
+            print(f"Valeur ADC : {self.valeurADC}, Voltage :{self.voltage:.2}")
+            sleep(0.03)
+            
+    def lcd(self, valeur):
+        self.ecran_lcd.write(0, 0, valeur)
+        
+    def fermer_programme(self):
+        self.ouvert = False
+    
+    def ouvrir_programme(self):
+        self.ouvert = True
+    
+    def demarrer(self):
+        self.bouton_demarrer.wait_for_press()
+        self.ouvrir_programme()
+        self.potentionmetre()
